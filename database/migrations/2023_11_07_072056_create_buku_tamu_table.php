8dfca7a65389cc6dcf5d9761ec9290180537d7fb
hadyanaa <hadyanabdulaziz@gmail.com>
2023-11-07 14:57:46 +0700

    push 2

---
+++ database/migrations/2023_11_07_072056_create_buku_tamu_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buku_tamu', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nama');
+            $table->string('email');
+            $table->string('password');
+            $table->string('phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buku_tamu');
+    }
+};
